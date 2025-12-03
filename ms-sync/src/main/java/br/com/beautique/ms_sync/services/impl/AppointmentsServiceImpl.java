2ddf527ddb0f2550732f454b2d9995e49ae95fa9
Juliana Michelsen <jiic@gft.com>
2025-12-03 15:26:53 -0300

    Cria as interfaces do service e repository de ms-sync

---
+++ ms-sync/src/main/java/br/com/beautique/ms_sync/services/impl/AppointmentsServiceImpl.java
@@ -0,0 +1,23 @@
+package br.com.beautique.ms_sync.services.impl;
+
+import br.com.beautique.ms_sync.dtos.appointments.FullAppointmentDTO;
+import br.com.beautique.ms_sync.dtos.beautyprocedures.BeautyProcedureDTO;
+import br.com.beautique.ms_sync.dtos.customers.CustomerDTO;
+import br.com.beautique.ms_sync.services.AppointmentService;
+
+public class AppointmentsServiceImpl implements AppointmentService {
+    @Override
+    public void saveAppointment(FullAppointmentDTO appointment) {
+
+    }
+
+    @Override
+    public void updateAppointmentCustomer(CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void updateAppointmentBeautyProcedure(BeautyProcedureDTO beautyProcedureDTO) {
+
+    }
+}
